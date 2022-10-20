fb6abbb58ebff48204a537936f17d23f44b263a8
Mirac <115193668+cure911@users.noreply.github.com>
2022-10-19 20:04:27 -0400

    Add files via upload

---
+++ close-road/script.js
@@ -0,0 +1,5 @@
+function StageTwo() {
+    document.getElementsByClassName("panel")[0].style.display = "none";
+    document.body.style.backgroundColor = "white";
+    document.getElementsByClassName("main-side")[0].style.display = "block";
+}
